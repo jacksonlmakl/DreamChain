c9fd12076fe73ef12f6a220b97b9082095839dd3
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-10 14:37:22 -0400

    Update master_node.py

--- master_node.py
+++ master_node.py
@@ -2,3 +2,13 @@ from dreamchain import DreamChainNode ,MasterNode, Node
 
 if __name__ == '__main__':
     master_node = MasterNode()
+    # Create a local node that connects to the master node at localhost:5000
+    node = Node(5001, master_node)
+
+    # Add a transaction and mine a block
+    node.add_transaction('Jackson', 'Jackson', 'Some Data')
+    node.mine_block()
+    node.resolve_conflicts()
+    while True:
+        continue
+    
