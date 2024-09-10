bda9c1abab9cbd489b87c4436ed0aed41023de1d
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-10 18:27:17 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Read the contents of your README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='DreamChain',
+    version='0.01',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'pickle',
+        'socket',
+        'flask'
+        
+    ],
+    author='Jackson Makl',
+    author_email='jlm487@georgetown.edu',
+    description='A Python client to interact with the DataPlatform API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify Markdown here
+    url='https://github.com/jacksonlmakl/DataPlatform',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
