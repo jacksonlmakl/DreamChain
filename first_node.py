890307754462d532b7e14513f4c9fc925dc9f3a1
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-10 17:52:04 -0400

    Create first_node.py

---
+++ first_node.py
@@ -0,0 +1,10 @@
+from dreamchain import DreamChainNode
+
+# Create and connect Node 1
+node1 = DreamChainNode(5005)
+
+# Node 1 mines a block
+node1.add_transaction('Jackson', 'Jackson', {'Data':'Something'})
+node1.mine_block()
+while True:
+  pass
