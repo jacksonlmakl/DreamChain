e2749179a11b87068792244d9b2b4ece7a20b6ce
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-11 18:59:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='DreamChain',
-    version='0.21',
+    version='0.22',
     packages=find_packages(),
     install_requires=[
         'requests',
