f9d51598a455759333eac595fed1168502301d25
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-10 20:19:55 -0400

    Update start.sh

--- DreamChain/start.sh
+++ DreamChain/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup python3 master_node.py & > master_node.log 2>&1 &
-nohup python3 first_node.py & > first_node.log 2>&1 &
+nohup python3 master_node.py > master_node.log 2>&1 &
+nohup python3 first_node.py > first_node.log 2>&1 &
 
 echo "master_node.py is now running in the background. See master_node.log for logs"
 echo "first_node.py is now running in the background. See first_node.log for logs"
